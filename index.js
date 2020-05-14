7e4c10fff4ddae128c945c2c1d99e1246669d518
nikita202 <nikita@ostaplyuk.com>
2020-05-14 12:09:37 +0300

    shit fixed20

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function start() {
                 }
                 let name = arguments.shift().value;
                 let id = arguments.shift().value;
-                if (!id.match(/^[^?&"'>]+$/)) {
+                if (!id.match(/^[a-zA-Z0-9-_]{11}$/)) {
                     msg.reply(`${id} is not a youtube video id`);
                     return;
                 }
@@ -150,7 +150,7 @@ function start() {
                 }
                 let name = arguments.shift().value;
                 let id = arguments.shift().value;
-                if (!id.match(/^[^?&"'>]+$/)) {
+                if (!id.match(/^[a-zA-Z0-9-_]{11}$/)) {
                     msg.reply(`${id} is not a youtube video id`);
                     return;
                 }
